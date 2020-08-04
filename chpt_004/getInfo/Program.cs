1fd9930c35f01db410667432c760b75b602833d9
raddevus <1radeutsch1@gmail.com>
2020-08-04 14:42:21 -0400

    initial creation of project

---
+++ chpt_004/getInfo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace getInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1){
+                Console.WriteLine("Please provide 1 argument to indicate the command you want to run.\nUsage: getInfo <command-name>");
+                return;
+            }
+
+            switch (args[0]){
+                case "os":{
+                    Console.WriteLine($"OS : {Environment.OSVersion}");
+                    break;
+                }
+
+            }
+        }
+    }
+}
