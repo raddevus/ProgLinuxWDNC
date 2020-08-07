b6af7f7878f3a1dd11593e237c2073c475023bcf
raddevus <1radeutsch1@gmail.com>
2020-08-07 11:47:47 -0400

    Updated ignore to ignore .txt files, calling AppendAllText().

--- chpt_005/fileWriter/Program.cs
+++ chpt_005/fileWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace fileWriter
 {
@@ -6,7 +7,7 @@ namespace fileWriter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            File.AppendAllText("test.txt","Hello World!");
         }
     }
 }
