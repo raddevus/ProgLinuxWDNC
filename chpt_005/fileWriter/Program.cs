b38e327833b32942d76b6bb433f7f8c763306c7f
raddevus <1radeutsch1@gmail.com>
2020-08-07 11:25:31 -0400

    Initial creation of project

---
+++ chpt_005/fileWriter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fileWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
