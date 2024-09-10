427565f08c89d6a6a42c9074c84a0537e30d7d3a
raddevus <1radeutsch1@gmail.com>
2024-09-10 14:30:58 -0400

    UPdated to net8.0

---
+++ chpt_002/FirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FirstApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String output = args[0];
+        Console.WriteLine(output);
+        var argIndex = 0;
+        foreach(String s in args){
+            Console.WriteLine($"args[{argIndex}]: {s}");
+            argIndex++;
+        }
+        Console.WriteLine("Hello, World!");
+    }
+}
